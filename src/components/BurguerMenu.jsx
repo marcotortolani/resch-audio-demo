52b00d1a6121522eee5550727add3ed9c3c72afc
Marco Tortolani <mjtorto@gmail.com>
2023-10-18 21:27:18 -0300

    fix imports

--- src/components/BurguerMenu.jsx
+++ src/components/BurguerMenu.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import SocialNetFooter from "@/components/SocialNetFooter";
+import { Button } from "../components/ui/button";
+import SocialNetFooter from "../components/SocialNetFooter";
 import {
   Sheet,
   SheetClose,
